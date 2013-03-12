3a6d628b56981800e844c9c21645a1bb05280050
Yann TCHOB <yann.tchobdrenovitch@orange.fr>
2013-03-12 22:09:13 +0100

    projet BTS v1

--- controleurs/c_consulterPraticiens.php
+++ controleurs/c_consulterPraticiens.php
@@ -1,7 +1,5 @@
 <?php
+include("vues/v_sommaire.php");
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 ?>
--- vues/v_saisirCR.php
+++ vues/v_saisirCR.php
@@ -2,3 +2,47 @@
 //include("vues/v_sommaire.php"); Pas besoin car déjà dans le controleur
 
 ?>
+
+<div id="contenu">
+      <h2>Rapport de visite </h2>
+      <div class="corpsForm">
+        <table>
+  	   <caption>Saisie des compte rendu de visite</caption>
+             <tr>
+                <td>Numero : </td>  
+                <td><input type="text" name="num" spaceholder="Votre numéro"></td>           
+             </tr>
+             <tr>
+                <td>Date visite : </td>  
+                <td><input type="text" name="date"></td>           
+             </tr>
+             <tr>
+                <td>Praticien : </td>  
+                <td></td>           
+             </tr>
+             <tr>
+                <td>Coefficient : </td>  
+                <td></td>           
+             </tr>
+             <tr>
+                <td>Remplacant : </td>  
+                <td></td>           
+             </tr>
+             <tr>
+                <td>Date : </td>  
+                <td></td>           
+             </tr>
+             <tr>
+                <td>Motif : </td>  
+                <td></td>           
+             </tr>
+             <tr>
+                <td>Bilan : </td>  
+                <td></td>           
+             </tr>
+                                          
+        </table>
+      </div>
+</div>
+     
+  
