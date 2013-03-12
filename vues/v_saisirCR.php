c505e3162e102b85f62310110cd3717e1d19077d
Yann TCHOB <yann.tchobdrenovitch@orange.fr>
2013-03-12 22:16:52 +0100

    projet BTS v1

--- vues/v_saisirCR.php
+++ vues/v_saisirCR.php
@@ -6,7 +6,7 @@
 <div id="contenu">
       <h2>Rapport de visite </h2>
       <div class="corpsForm">
-        <table>
+        <table border="0">
   	   <caption>Saisie des compte rendu de visite</caption>
              <tr>
                 <td>Numero : </td>  
@@ -14,31 +14,41 @@
              </tr>
              <tr>
                 <td>Date visite : </td>  
-                <td><input type="text" name="date"></td>           
+                <td><input type="text" name="dateVisite"></td>           
              </tr>
              <tr>
                 <td>Praticien : </td>  
-                <td></td>           
+                <td><SELECT name="praticien" size="1">
+                    <OPTION>lundi
+                    <OPTION>mardi
+                    <OPTION selected>mercredi
+                    <OPTION>jeudi
+                    <OPTION>vendredi
+                    </SELECT></td>           
              </tr>
              <tr>
                 <td>Coefficient : </td>  
-                <td></td>           
+                <td><input type="text" name="coeff"></td>           
              </tr>
              <tr>
                 <td>Remplacant : </td>  
-                <td></td>           
+                <td><input type="checkbox" name="remplacant" value="yes"></td>           
              </tr>
              <tr>
                 <td>Date : </td>  
-                <td></td>           
+                <td><input type="text" name="date"></td>           
              </tr>
              <tr>
                 <td>Motif : </td>  
-                <td></td>           
+                <td><SELECT name="praticien" size="1">
+                    <OPTION selected>Périodicité
+                    <OPTION>
+                    <OPTION>
+                    </SELECT></td>
              </tr>
              <tr>
                 <td>Bilan : </td>  
-                <td></td>           
+                <td><textarea rows="5" cols="50"></textarea></td>           
              </tr>
                                           
         </table>
