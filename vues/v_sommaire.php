091cd6fc216454c66d649af5036d62d26be1a0cc
Yann TCHOB <yann.tchobdrenovitch@orange.fr>
2013-03-13 17:06:51 +0100

    projet BTS v1

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -11,26 +11,29 @@
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
               <li class="smenu">
-                   Comptes rendus 
-           </li> 
-           <li class="smenu"><!-- Lien vers les controleurs et les actions (s'il y en a) -->
-              <a href="index.php?uc=gererCR&action=saisirCR" title="Nouveaux comptes rendus">Nouveaux</a>
-           </li>
-           <li class="smenu">
-              <a href="index.php?uc=gererCR&action=consulterCR" title="Consulter les comptes rendus">Consulter</a>
-           </li><li class="smenu">
-           <li class="smenu">
-              Consultations
-           </li>
-           <li class="smenu">
-              <a href="index.php?uc=consulterMedicament" title="Consulter les médicaments">Médicaments</a>
-           </li><li class="smenu">
-              <a href="index.php?uc=consulterPraticien" title="Consulter les praticiens">Praticiens</a>
-           </li>
-           <li class="smenu">
-              <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
-           </li>
-         </ul>
-        
+                   Comptes rendus
+              </li> 
+           <ul>
+               <li class="smenu"><!-- Lien vers les controleurs et les actions (s'il y en a) -->
+                    <a href="index.php?uc=gererCR&action=saisirCR" title="Nouveaux comptes rendus">Nouveaux</a>
+               </li>
+               <li class="smenu">
+                   <a href="index.php?uc=gererCR&action=consulterCR" title="Consulter les comptes rendus">Consulter</a>
+               </li>
+           </ul>
+              <li class="smenu">Consultations</li>
+              
+           <ul>
+              <li class="smenu">
+                  <a href="index.php?uc=consulterMedicament" title="Consulter les médicaments">Médicaments</a>
+              </li>
+              <li class="smenu">
+                  <a href="index.php?uc=consulterPraticien" title="Consulter les praticiens">Praticiens</a>
+              </li>
+              <li class="smenu">
+                  <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
+              </li>
+           </ul>
+              
+        </ul>
     </div>
-    
