d7c523ff097e6fa84d85b1e0fcf22afd12ec0f27
Mathieu Muller <matmullerdev@gmail.com>
2013-03-15 10:57:36 +0100

    Ajout vue praticienParville et praticien par détail, ajout fct lesInfosPraticiensParVille et fct recup info par idPraticien

--- vues/v_lesPraticiens.php
+++ vues/v_lesPraticiens.php
@@ -3,11 +3,11 @@
     <h2>Details Praticiens</h2>
     <a href="index.php?uc=consulterPraticien&action=parVille"><input type="button" value="Trier Par Ville"></a>
     <table>
-        <tr><th>Nom</th><th>Prenom</th><th>Adresse</th><th>Ville</th><th>En savoir plus</th><!--<th>Fonction</th><th>Lieu</th><th>Coeff</th>--></tr>        
+        <tr><th>Nom</th><th>Prenom</th><th>Adresse</th><th>Ville</th><th>En savoir plus</th></tr>        
 <?php
 foreach($lesInfosPraticiens as $unPraticien){
 ?>
-        <tr><td><?php echo $unPraticien['PRA_NOM'];?></td><td><?php echo $unPraticien['PRA_PRENOM'];?></td><td><?php echo $unPraticien['PRA_ADRESSE'];?></td><td><?php echo $unPraticien['PRA_VILLE'];?></td><td><a href="index.php?uc=consulterPraticien&action=lePraticien&id=<?php echo $unPraticien['PRA_NUM']; ?>">+<?php echo $unPraticien['PRA_NUM']; ?><a/></td></tr>
+        <tr><td><?php echo $unPraticien['PRA_NOM'];?></td><td><?php echo $unPraticien['PRA_PRENOM'];?></td><td><?php echo $unPraticien['PRA_ADRESSE'];?></td><td><?php echo $unPraticien['PRA_VILLE'];?></td><td><a href="index.php?uc=consulterPraticien&action=lePraticien&id=<?php echo $unPraticien['PRA_NUM']; ?>"><center>+</center><a/></td></tr>
         
 <?php } ?>
     </table>
--- vues/v_lesPraticiensVille.php
+++ vues/v_lesPraticiensVille.php
@@ -2,11 +2,11 @@
     
     <h2>Details Praticiens</h2>
     <table>
-        <tr><th>Ville</th><th>Nom</th><th>Prenom</th><th>Adresse</th><th>En savoir plus</th><!--<th>Fonction</th><th>Lieu</th><th>Coeff</th>--></tr>        
+        <tr><th>Ville</th><th>Nom</th><th>Prenom</th><th>Adresse</th><th>En savoir plus</th></tr>        
 <?php
 foreach($lesInfosPraticiens as $unPraticien){
 ?>
-        <tr><td><?php echo $unPraticien['PRA_VILLE'];?></td><td><?php echo $unPraticien['PRA_NOM'];?></td><td><?php echo $unPraticien['PRA_PRENOM'];?></td><td><?php echo $unPraticien['PRA_ADRESSE'];?></td><td><a href="index.php?uc=consulterPraticien&action=lePraticien&id=<?php echo $unPraticien['PRA_NUM'] ?>">+<a/></td><!--<td><?php echo $unPraticien['TYP_LIBELLE'];?></td><td><?php echo $unPraticien['TYP_LIEU'];?></td><td><?php echo $unPraticien['PRA_COEFNOTORIETE'];?></td>--></tr>
+        <tr><td><?php echo $unPraticien['PRA_VILLE'];?></td><td><?php echo $unPraticien['PRA_NOM'];?></td><td><?php echo $unPraticien['PRA_PRENOM'];?></td><td><?php echo $unPraticien['PRA_ADRESSE'];?></td><td><a href="index.php?uc=consulterPraticien&action=lePraticien&id=<?php echo $unPraticien['PRA_NUM'] ?>">+<a/></td></tr>
         
 <?php } ?>
     </table>
