dee6021cd49a297f6d77058324cec025c3e7b00d
Mathieu Muller <matmullerdev@gmail.com>
2013-03-15 10:50:01 +0100

    Ajout vue praticien, ajout fct lesInfosPraticiens

--- controleurs/c_consulterPraticiens.php
+++ controleurs/c_consulterPraticiens.php
@@ -1,6 +1,29 @@
 <?php
-include("vues/v_sommaire.php");
-$lesInfosPraticiens = $pdo->getLesInfosPraticiens();
-include("vues/v_lesPraticiens.php");
-
+if(!isset($_REQUEST['action'])){
+	$_REQUEST['action'] = 'liste';
+}
+$action = $_REQUEST['action'];
+switch($action){
+	case 'liste':{		
+            include("vues/v_sommaire.php");
+            $lesInfosPraticiens = $pdo->getLesInfosPraticiens();
+            include("vues/v_lesPraticiens.php");
+		break;
+	}
+	case 'parVille':{
+                include("vues/v_sommaire.php");
+            $lesInfosPraticiens = $pdo->getLesInfosPraticiensParVille();
+            include("vues/v_lesPraticiensVille.php");
+		break;
+	
+         }
+         case 'lePraticien':{
+                include("vues/v_sommaire.php");
+                $idPraticien = $_REQUEST['id'];
+            $lesInfosPraticien = $pdo->getInfosPraticien($idPraticien);
+            include("vues/v_lePraticienDetail.php");
+		break;
+	
+         }
+}
 ?>
