adf5689536cb4f9fd755bfcbd828f78f2bd3ee01
Hasan Karagoz <hasan-saka@hotmail.fr>
2013-03-21 14:22:15 +0100

    version finale

--- controleurs/c_consulterMedicaments.php
+++ controleurs/c_consulterMedicaments.php
@@ -1,35 +1,13 @@
 <?php
 
 
-if(!isset($_REQUEST['action']) || $_REQUEST['action']!='suivant' || $_REQUEST['action']!='precedent')
-    {
-	$_REQUEST['action'] = 'debut';
-    }
-    
 $lesMedicaments=$pdo->getLesMedicaments();
-$action = $_REQUEST['action'];
 
 
-switch($action)
-{
-	case 'debut':
-        {
-           
-            include("vues/v_sommaire.php");
-            include("vues/v_lesMedicaments.php");
-            break;
-	}
-	case 'suivant':
-        {
-            include("vues/v_sommaire.php");
-            include("vues/v_lesMedicaments.php");
-            break;
-        }
-            case 'precedent':
-        {
-            include("vues/v_sommaire.php");
-            include("vues/v_lesMedicaments.php");
-            break;
-        }
-}
+
+
+include("vues/v_sommaire.php");
+include("vues/v_lesMedicaments.php");
+       
+
 ?>
